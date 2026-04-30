91f75cab02364aed6ae4ef911bbe21d49d0a6f19
mauriisalas66-art <mauriisalas66@gmail.com>
2026-04-29 22:01:03 -0300

    @SalasMauri66:Agregue los ejercicios 2,6,10,16

--- src/HolajssSalasMauricio.js
+++
@@ -1,9 +0,0 @@
-
-var nombre = "Mauricio Rodrigo";
-
-let apellido = "Salas";
-const Texto = `Hola Mundo, segun  ${nombre} ${apellido}`;
-
-//console.log("Hola Mundo, segun " + nombre + apellido);
-//console.log(Texto + nombre + apellido );
-console.log( Texto );
---
+++ src/js/ejercicio2.js
@@ -0,0 +1,9 @@
+//2. Mostrar por consola los primeros 10 números impares.
+let contador = 0;
+let numero = 1;
+
+while (contador < 10) {
+  console.log(numero);
+  numero += 2;
+  contador++;
+}
