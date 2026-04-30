33d32f6b0f98aaccb20c51e12f9c036a01b2b9ab
nievesales <nievesaless45@gmail.com>
2026-04-30 13:41:58 -0300

    ejercicio 4 Luis Nieves

---
+++ assets/SRC/holajsLuisNieves.js
@@ -0,0 +1,14 @@
+// no usar
+var = "Luis Nieves";
+// trabajar con let y const
+let apellido = "Nieves";
+// const Texto = "Hola Mundo segun ";
+const Texto = ' "Hola Mundo segun  ${nombre} ${apellido} ';
+// const Texto = `Hola Mundo segun ${nombre} ${apellido} `;
+
+// console.log("Hola Mundo segun " + nombre + apellido);
+
+// console.log(TEXTO + nombre + apellido);
+
+
+console.log( Texto );
